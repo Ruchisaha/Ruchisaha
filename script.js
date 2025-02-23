05bb7031554052386b9ac0c2348c0a140dac110c
Ruchisaha <ruchikumari24@navgurukul.org>
2025-02-23 06:29:24 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-let input =document.getElementById("inp");
-let text=document.querySelector(".text");
-
-function Add(){
-    if(input.value==""){
-        alert("please Enter task")
-    }else{
-        let newEle=document.createElement("li");
-        newEle.innerHTML = `${input.value}  <i class="fa-solid fa-trash"></i>`; 
-        text.appendChild(newEle);
-        input.value=""
-        newEle.querySelector("i").addEventListener("click",function(){
-        
-            newEle.remove();
-        });
+function setReminder() {
+    let timeInput = document.getElementById("reminder-time").value;
+    let reminderText = document.getElementById("reminder-text").value;
+    let reminderImage = document.getElementById("reminder-image").files[0];
+    
+    if (!timeInput || !reminderText) {
+        alert("Please enter both time and reminder note!");
+        return;
     }
+
+    let currentTime = new Date();
+    let reminderTime = new Date();
+    let [hours, minutes] = timeInput.split(":");
+    reminderTime.setHours(hours, minutes, 0, 0);
+
+    let timeDiff = reminderTime - currentTime;
+
+    if (timeDiff < 0) {
+        alert("Selected time is in the past. Please select a future time!");
+        return;
+    }
+
+    setTimeout(() => {
+        document.getElementById("alert-text").innerText = `⏰ ${reminderText} ⏰`;
+        document.getElementById("alert").classList.remove("hidden");
+
+        let alarmSound = document.getElementById("alarm-sound");
+        alarmSound.play();
+
+        if (reminderImage) {
+            let imgURL = URL.createObjectURL(reminderImage);
+            document.getElementById("reminder-photo").src = imgURL;
+        }
+    }, timeDiff);
+}
+
+function stopAlarm() {
+    document.getElementById("alert").classList.add("hidden");
+    let alarmSound = document.getElementById("alarm-sound");
+    alarmSound.pause();
+    alarmSound.currentTime = 0; // Reset audio to start
 }
